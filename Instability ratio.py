f2d287349a79b6018cccf1b69a995013349e6b55
pmollaei <122331379+pmollaei@users.noreply.github.com>
2023-08-14 11:51:17 -0400

    Instability_ratio.py

--- Instability ratio.py
+++ Instability ratio.py
@@ -1,23 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import mdtraj as md
-import numpy as np
-import pandas as pd
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 from itertools import groupby
-import numpy
-import glob
-import warnings
-warnings.filterwarnings("ignore")
-
-
-# In[9]:
-
 
 data = 'angle dataset'
 clustering = KMeans(2).fit_predict(data.reshape(-1, 1))
